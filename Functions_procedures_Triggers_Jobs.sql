9236e3dde62ad382eadb0b61a594797351700f11
AngieInstitucional <angie.cordoba.castro@est.una.ac.cr>
2021-10-19 13:45:13 -0600

    script y triggers actualizados

--- Functions_procedures_Triggers_Jobs.sql
+++ Functions_procedures_Triggers_Jobs.sql
@@ -32,11 +32,11 @@ declare
   cuenta_proveedor number;
   pragma autonomous_transaction;
 begin
-  select c.cue_id into cuenta_proveedor from PEV_CUENTAS_PAGAR c where c.cue_proveedor = :new.fac_provedor;
+  select c.cue_id into cuenta_proveedor from PV_CUENTAS_PAGAR c where c.cue_proveedor = :new.fac_provedor;
   
   begin
     if(:new.fac_estado = 'P') then
-      update PEV_CUENTAS_PAGAR c
+      update PV_CUENTAS_PAGAR c
       set c.cue_monto = (c.cue_monto + :new.fac_monto_total)
       where c.cue_id = cuenta_proveedor;
       commit;
@@ -51,22 +51,22 @@ end TRI_AUMENTAR_CUENTA_PAGAR;
 
 create or replace noneditionable trigger TRI_CAMBIAR_ESTADO_FAC_VENTA
   after insert
-  on pv_abonos_ventas 
+  on pv_abonos_ventas
   for each row
 declare
   -- local variables here
   abonos float;
   montoTotal float;
   pragma autonomous_transaction;
-  
+
 begin
   begin
-    select f.fac_total into montoTotal from PV_FACTURAS_VENTA f where f.fac_id = :new.fac_id;
-    select SUM(a.abov_monto) into abonos from PV_ABONOS_VENTAS a where a.fac_id = :new.fac_id;
+    select f.fac_total into montoTotal from PV_FACTURAS_VENTA f where f.fac_id = :new.abov_fac_id;
+    select SUM(a.abov_monto) into abonos from PV_ABONOS_VENTAS a where a.abov_fac_id = :new.abov_fac_id;
 
     if (abonos = montoTotal) then
       begin
-        UPDATE PV_FACTURAS_VENTA f set f.fac_estado = 'C' where f.fac_id = :new.fac_id;
+        UPDATE PV_FACTURAS_VENTA f set f.fac_estado = 'C' where f.fac_id = :new.abov_fac_id;
         commit;
       end;
     end if;
@@ -78,9 +78,9 @@ end TRI_CAMBIAR_ESTADO_FAC_VENTA;
 
 --Trigger de cambiar el estado de las facturas de compra a canceladas
 
-create or replace trigger TRI_CAMBIAR_ESTADO_FACT_COMPRA
+create or replace noneditionable trigger TRI_CAMBIAR_ESTADO_FACT_COMPRA
   after insert
-  on pv_abonos 
+  on pv_abonos
   for each row
 declare
 
@@ -89,17 +89,17 @@ declare
   pragma autonomous_transaction;
 begin
   begin
-    select f.fac_monto_total into montoTotal from PV_FACTURAS_COMPRA f where f.fac_id = :new.fac_id;
-    select SUM(a.abo_monto) into abonos from PV_ABONOS a where a.fac_id = :new.fac_id;
+    select f.fac_monto_total into montoTotal from PV_FACTURAS_COMPRA f where f.fac_id = :new.abo_fac_id;
+    select SUM(a.abo_monto) into abonos from PV_ABONOS a where a.abo_fac_id = :new.abo_fac_id;
 
     if (abonos = montoTotal) then
       begin
-        UPDATE PV_FACTURAS_COMPRA f set f.fac_estado = 'C' where f.fac_id = :new.fac_id;
+        UPDATE PV_FACTURAS_COMPRA f set f.fac_estado = 'C' where f.fac_id = :new.abo_fac_id;
         commit;
       end;
     end if;
-  end;  
-    
+  end;
+
 end TRI_CAMBIAR_ESTADO_FACT_COMPRA;
 /
 
@@ -130,7 +130,7 @@ declare
   -- local variables here
 begin
   begin
-     insert into PEV_CUENTAS_PAGAR(CUE_PROVEEDOR,CUE_MONTO) VALUES(:new.pro_id,0);
+     insert into PV_CUENTAS_PAGAR(CUE_PROVEEDOR,CUE_MONTO) VALUES(:new.pro_id,0);
 
   end;
 end TRI_CREAR_CUENTA_GENERAL_EMPRESA;
@@ -147,11 +147,11 @@ declare
   -- local variables here
   cuentaProveed number;
 begin
-  select f.fac_provedor into cuentaProveed from PV_FACTURAS_COMPRA f where f.fac_id = :new.fac_id;
+  select f.fac_provedor into cuentaProveed from PV_FACTURAS_COMPRA f where f.fac_id = :new.abo_fac_id;
   
   begin
     
-     update PEV_CUENTAS_PAGAR c set c.cue_monto = (c.cue_monto - :new.abo_monto) where c.cue_proveedor = cuentaProveed;
+     update PV_CUENTAS_PAGAR c set c.cue_monto = (c.cue_monto - :new.abo_monto) where c.cue_proveedor = cuentaProveed;
 
   end;
 end TRI_DISMINUIR_CUENTAS_PAGAR;
@@ -168,7 +168,7 @@ declare
   -- local variables here
   cuentaCliente number;
 begin
-   select f.fac_cliente into cuentaCliente from PV_FACTURAS_VENTA f where f.fac_id = :new.fac_id;
+   select f.fac_cliente into cuentaCliente from PV_FACTURAS_VENTA f where f.fac_id = :new.abov_fac_id;
   
   begin
     
